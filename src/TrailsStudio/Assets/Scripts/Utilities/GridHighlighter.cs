1bd4918d25ea8b93351c011fe7122946c28a9a71
Martin Křen <martin.kren165@gmail.com>
2024-10-23 16:06:24 +0200

    add distance measure in build phase

--- src/TrailsStudio/Assets/Scripts/Utilities/GridHighlighter.cs
+++ src/TrailsStudio/Assets/Scripts/Utilities/GridHighlighter.cs
@@ -4,8 +4,7 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
-// ZEPTAT SE
-// jak udelat grid highlighter, aby dokazal obepinat i komplikovanej teren? DECALS
+// TODO jak udelat grid highlighter, aby dokazal obepinat i komplikovanej teren? DECALS
 
 [RequireComponent(typeof(LineRenderer))]
 public class GridHighlighter : MonoBehaviour
@@ -15,23 +14,28 @@ public class GridHighlighter : MonoBehaviour
     public LineRenderer lineRenderer;
     public GameObject distanceMeasure;
 
-
     private GameObject highlight;
+    private Transform rollInTransform;
+
+    private void Initialize()
+    {
+        highlight = Instantiate(highlightPrefab);
+        highlight.SetActive(false);
+        distanceMeasure.SetActive(true);
+        lineRenderer.material = new Material(Shader.Find("Unlit/Color"));
+        lineRenderer.material.color = Color.black;
+        rollInTransform = Line.Instance.line[0].transform;
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        highlight = Instantiate(highlightPrefab);
-        highlight.SetActive(false);
-        distanceMeasure.SetActive(true);
-        
+        Initialize();
     }
 
     private void OnEnable()
     {
-        highlight = Instantiate(highlightPrefab);
-        highlight.SetActive(false);
-        distanceMeasure.SetActive(true);
+        Initialize();
     }
 
     // Update is called once per frame
@@ -56,15 +60,19 @@ public class GridHighlighter : MonoBehaviour
                 // make the text go a bit higher so that it does not clip through the terrain
                 distanceMeasure.transform.position += new Vector3(0, 0.1f, 0);
 
-                // TODO rotate so that it is along the line, not across the line
-                distanceMeasure.transform.rotation = Quaternion.FromToRotation(Vector3.up, (hitPoint - Line.Instance.currentLineEndPoint).normalized);
+                // make the text face the camera and position it along the line
+                Transform camTransform = CameraManager.Instance.GetTopDownCamTransform();
+                distanceMeasure.transform.LookAt(camTransform, camTransform.up);
+                distanceMeasure.transform.Rotate(0, 180, 0);
 
                 distanceMeasure.GetComponent<TextMeshPro>().text = $"Distance: {distance:F2}m";
-                
-                // TODO solve the material problem!!
+
+                // draw a line between the current line end point and the point where the mouse is pointing
                 lineRenderer.positionCount = 2;
                 lineRenderer.SetPosition(0, Line.Instance.currentLineEndPoint);
+                //lineRenderer.SetPosition(1, Line.Instance.currentLineEndPoint + (Vector3.Project(rollInTransform.forward, terrain.transform.right) * 50));
                 lineRenderer.SetPosition(1, hitPoint);
+                
 
             }
 
