cfae284f7682d6eac999927614ff4cff09cec495
Martin Křen <martin.kren165@gmail.com>
2025-01-03 21:26:04 +0100

    implement click&drag detail view camera for build phases

---
+++ src/TrailsStudio/Assets/Scripts/InputSystem/ClickAndDragInputHandler.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Unity.Cinemachine;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using static Unity.Cinemachine.CinemachineInputAxisController.Reader;
+
+
+public class ClickAndDragInputHandler : MonoBehaviour
+{
+    private CinemachineInputAxisController inputAxisController;
+
+    void Start()
+    {
+        inputAxisController = GetComponent<CinemachineInputAxisController>();
+        if (inputAxisController != null)
+        {
+            inputAxisController.ReadControlValueOverride = CustomReadControlValue;
+        }
+    }
+
+    private float CustomReadControlValue(
+    InputAction action, IInputAxisOwner.AxisDescriptor.Hints hint, UnityEngine.Object context,
+    ControlValueReader defaultReader)
+    {
+        var control = action.activeControl;
+        if (control != null)
+        {
+            try
+            {
+                // If we can read as a Vector2, do so
+                if (control.valueType == typeof(Vector2) || action.expectedControlType == "Vector2")
+                {
+                    var value = action.ReadValue<Vector2>();
+                    return hint == IInputAxisOwner.AxisDescriptor.Hints.Y ? value.y : value.x;
+                }
+                // Default: assume type is float
+                return action.ReadValue<float>();
+            }
+            catch (InvalidOperationException)
+            {
+                Debug.LogError($"An action in {context.name} is mapped to a {control.valueType.Name} "
+                    + "control.  CinemachineInputAxisController.Reader can only handle float or Vector2 types.  "
+                    + "To handle other types you can install a custom handler for "
+                    + "CinemachineInputAxisController.ReadControlValueOverride.");
+            }
+        }
+        return 0f;
+    }
+}
---
+++ src/TrailsStudio/Assets/Scripts/InputSystem/SwapAxisProcessor.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using System;
+using UnityEditor;
+using UnityEngine.InputSystem;
+
+
+[Serializable]
+public class SwapAxisProcessor : InputProcessor<Vector2>
+{
+    [Tooltip("send x-axis to y-axis and vise versa")]
+    public bool swapAxis = true;
+
+    public override Vector2 Process(Vector2 value, InputControl control)
+    {
+        return swapAxis ? new(value.y, value.x) : value;
+    }
+
+#if UNITY_EDITOR
+    [InitializeOnLoadMethod]
+#endif
+    [RuntimeInitializeOnLoadMethod]
+    public static void Init()
+    {
+        Debug.Log("Initializing SwapAxisProcessor");
+        InputSystem.RegisterProcessor<SwapAxisProcessor>();
+    }
+}
