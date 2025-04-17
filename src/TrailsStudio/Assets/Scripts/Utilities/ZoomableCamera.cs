bfdf8a82e90dd505f998b6ee8ae344ec572f96c5
Martin Křen <martin.kren165@gmail.com>
2025-04-17 15:07:07 +0200

    add zoomable camera script

---
+++ src/TrailsStudio/Assets/Scripts/Utilities/ZoomableCamera.cs
@@ -0,0 +1,41 @@
+﻿
+using System.Collections;
+using Unity.Cinemachine;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Assets.Scripts.Utilities
+{
+    /// <summary>
+    /// Manipulates FOV of a <see cref="CinemachineCamera"/> to zoom in and out using user input.
+    /// </summary>
+    [RequireComponent(typeof(CinemachineCamera))]
+    public class ZoomableCamera : MonoBehaviour
+    {
+        [SerializeField] CinemachineCamera cam;
+        [SerializeField] float zoomSpeed = 1f;
+        [SerializeField] float minZoom = 20f;
+        [SerializeField] float maxZoom = 100f;
+
+        InputAction zoomAction;
+
+        private void OnEnable()
+        {
+            zoomAction = InputSystem.actions.FindAction("Zoom");
+            zoomAction.performed += OnZoom;
+        }
+
+        private void OnDisable()
+        {
+            zoomAction.performed -= OnZoom;
+        }
+
+        void OnZoom(InputAction.CallbackContext context)
+        {
+            if (cam == null) return;
+            float zoomChange = context.ReadValue<float>() * zoomSpeed * Time.deltaTime;
+            float finalZoom = Mathf.Clamp(cam.Lens.FieldOfView + zoomChange, minZoom, maxZoom);
+            cam.Lens.FieldOfView = finalZoom;
+        }
+    }
+}
